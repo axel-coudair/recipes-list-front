0f5b173c5b708df7ec29ae61294bb111e2648197
Axel Coudair <axel.coudair.dev@gmail.com>
2018-10-13 13:53:01 +0200

    :bug: fixing header errors

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar/Toolbar";
 import IconButton from "@material-ui/core/IconButton/IconButton";
 import Button from "@material-ui/core/Button/Button";
 import Typography from "@material-ui/core/Typography/Typography";
-import MenuIcon from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 // import './App.css';
 
 class Header extends Component {
