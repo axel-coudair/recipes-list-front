85968422176feb681897b6156e8e75c298ac249b
Axel Coudair <axel.coudair.dev@gmail.com>
2018-10-14 22:37:29 +0200

    ✨ ➕ add firebase, add login no ui not functional 2

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,6 +5,8 @@ import IconButton from "@material-ui/core/IconButton/IconButton";
 import Button from "@material-ui/core/Button/Button";
 import Typography from "@material-ui/core/Typography/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
+import Login from "../login";
+import Register from "../register";
 
 // import './App.css';
 
@@ -21,9 +23,10 @@ class Header extends Component {
                     <Typography variant="h6" color="inherit"
                         // className={classes.grow}
                     >
-                        News
+                        My Recipes List
                     </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Login/>
+                    <Register/>
                 </Toolbar>
             </AppBar>
         );
