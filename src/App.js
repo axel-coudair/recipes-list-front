436388bf8c0028020fff8832faf293703296486e
Axel Coudair <axel.coudair.dev@gmail.com>
2018-11-04 17:34:11 +0100

    :sparkles: working router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/header'
 import RecipesList from "./components/recipesList";
+import LoginButtonModal from "./components/loginButtonModal";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
     render() {
         return (
-            < div className="App">
-                <Header/>
-                <RecipesList/>
-            </div>
+
+            <BrowserRouter>
+                < div className="App">
+                    <Header />
+                    <Switch>
+                        <Route exact path='/' component={RecipesList} />
+                        {/* both /roster and /roster/:number begin with /roster */}
+                        <Route path='/roster' component={LoginButtonModal} />
+                    </Switch>
+                </div>
+
+            </BrowserRouter>
+
         );
     }
 }
