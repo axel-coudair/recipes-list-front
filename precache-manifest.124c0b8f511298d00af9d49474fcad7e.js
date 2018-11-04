d80dd208d5145c325eb9cb2f186641f26d150801
Axel Coudair <axel.coudair.dev@gmail.com>
2018-11-04 15:31:29 +0100

    Updates

---
+++ precache-manifest.124c0b8f511298d00af9d49474fcad7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15e9cd83579df5b5f6bd",
+    "url": "/recipes-list-front/static/js/runtime~main.15e9cd83.js"
+  },
+  {
+    "revision": "d4b331b14925573b9210",
+    "url": "/recipes-list-front/static/js/main.d4b331b1.chunk.js"
+  },
+  {
+    "revision": "16df39577eeb905a0877",
+    "url": "/recipes-list-front/static/js/1.16df3957.chunk.js"
+  },
+  {
+    "revision": "d4b331b14925573b9210",
+    "url": "/recipes-list-front/static/css/main.3eb1c703.chunk.css"
+  },
+  {
+    "revision": "f6c81c831922b2521222f262cf09b0b2",
+    "url": "/recipes-list-front/index.html"
+  }
+];
