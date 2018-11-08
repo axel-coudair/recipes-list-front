56c94ab1eef1ce3c36aeefabec3d0c41a598bdd1
Axel Coudair <axel.coudair.dev@gmail.com>
2018-11-08 09:57:26 +0100

    :sparkles: implementing Theme

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import Header from './components/header'
 import RecipesList from "./components/recipesList";
 import LoginButtonModal from "./components/loginButtonModal";
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import theme from './Theme'
 
 class App extends Component {
     render() {
         return (
 
             <BrowserRouter>
-                < div className="App">
-                    <Header />
-                    <Switch>
-                        <Route exact path='/' component={RecipesList} />
-                        {/* both /roster and /roster/:number begin with /roster */}
-                        <Route path='/roster' component={LoginButtonModal} />
-                    </Switch>
-                </div>
-
+                <MuiThemeProvider theme={theme}>
+                    < div className="App">
+                        <Header />
+                        <Switch>
+                            <Route exact path='/' component={RecipesList} />
+                            {/* both /roster and /roster/:number begin with /roster */}
+                            <Route path='/roster' component={LoginButtonModal} />
+                        </Switch>
+                    </div>
+                </MuiThemeProvider>
             </BrowserRouter>
 
         );
