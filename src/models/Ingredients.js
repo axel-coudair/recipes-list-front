cebd40f5586285d4cc5c9f3104064542ed0b4145
Axel Coudair <axel.coudair.dev@gmail.com>
2018-11-28 20:26:49 +0100

    :construction: Adding all the models

---
+++ src/models/Ingredients.js
@@ -0,0 +1,9 @@
+export default class Ingredients {
+	constructor(id, name, quantity, unit, checked) {
+		this.id = id
+		this.name = name
+		this.quantity = quantity
+		this.unit = unit
+		this.checked = checked
+	}
+}
